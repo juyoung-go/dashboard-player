a01b2d3c698e6c73928f47635e721be3dba0f887
고주영 <99633869+juyoung-go@users.noreply.github.com>
2022-11-03 20:22:20 +0900

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ app.on('ready', () => {
 
 //sonar test
 1 == 1
-1==1 && 2==2 && 3 == 3 //3 added to patch1 11111
+1==1 && 2==2 && 3 == 3 //3 added to patch1 11111 prpr
 app.on('window-all-closed', () => {
   app.quit()
 })
